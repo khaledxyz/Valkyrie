74a52853d2697546f19d86e3bf4df914d2723b0d
khaledxyz <attano001@outlook.com>
2022-06-07 15:32:02 +0100

    Added ChalkJS
    Customized the Console for the server side using ChalkJS.

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const chalk = require('chalk');
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log(`Server is live on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`
+    ${chalk.green('Dev server running at')}
+        > Local: ${chalk.blue(`http://localhost:${PORT}`)}
+        > Network: ${chalk.blue(`Not Enabled Yet.`)}
+`));
