08766ca542f90e709cafc9c554fe3d8f41c6bafd
khaledxyz <attano001@outlook.com>
2022-06-07 22:23:45 +0100

    initialized Express server.

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const chalk = require('chalk');
 require("dotenv").config();
 
@@ -11,11 +10,3 @@ app.listen(PORT, () => console.log(`
         > Local: ${chalk.blue(`http://localhost:${PORT}`)}
         > Network: ${chalk.blue(`Not Enabled Yet.`)}
 `));
-
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log(chalk.green(`   MongoDB Connected`))) 
-.catch(err => console.log(chalk.red(`${err})`)));
-// * The space before "MongoDB" is intentional, it is to align it in the Console.
