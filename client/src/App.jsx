ab9f675bbf256b36ec99794124f8f0b93e13ed41
khaledxyz <attano001@outlook.com>
2022-06-11 09:39:12 +0100

    Updated Button.jsx.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,10 @@
+import Button from './components/Button/Button';
+import { AiOutlinePlus } from 'react-icons/ai';
+import './sass/index.scss';
 const App = () => {
   return ( 
     <div className="App">
+      <Button variant={'normal'} color={'danger'} icon={<AiOutlinePlus />}>Danger</Button>
     </div>
    );
 }
--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,8 +1,28 @@
 import './Button.scss';
 
-const Button = ( { variant, color, value, icon } ) => {
+const SIZES = ['btn-md', 'btn-lg', 'btn-sm',];
+
+const Button = ( { type, onClick, variant, color, icon, children } ) => {
+    if(!type) type = 'button';
+    if(!onClick) onClick = () => {};
+
+    let STYLE = `btn-${variant}-${color}`;
+    if(STYLE.includes('undefined')) STYLE = "btn-normal-primary"
+
     return(
-        <button variant={variant} color={color}> {value} Primary {icon} </button>
+        <button 
+        type={type} 
+        onClick={(onClick)} 
+        className={`btn ${STYLE}`} >{children}{icon}</button>
+
+        // <button
+        // type={type}
+        // onClick={onClick}
+        // variant={variant} 
+        // icon={icon}
+        // color={color}>
+        // {children}
+        // </button>
     );
 }
  
