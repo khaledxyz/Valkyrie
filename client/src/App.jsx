7d3526e8ac136685f9de93d631e3faf4db670168
attano001 <attano001@outlook.com>
2022-06-11 10:06:19 +0100

    Finished Button component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,6 @@
-import Button from './components/Button/Button';
-import { AiOutlinePlus } from 'react-icons/ai';
-import './sass/index.scss';
 const App = () => {
   return ( 
     <div className="App">
-      <Button variant={'normal'} color={'danger'} icon={<AiOutlinePlus />}>Danger</Button>
     </div>
    );
 }
--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,28 +1,21 @@
 import './Button.scss';
 
-const SIZES = ['btn-md', 'btn-lg', 'btn-sm',];
+const SIZES = ['btn-md', 'btn-lg', 'btn-sm', 'btn-flexible'];
 
-const Button = ( { type, onClick, variant, color, icon, children } ) => {
+const Button = ( { type, onClick, variant, color, size, icon, children } ) => {
     if(!type) type = 'button';
     if(!onClick) onClick = () => {};
 
     let STYLE = `btn-${variant}-${color}`;
+    let SIZE = `btn-${size}`;
     if(STYLE.includes('undefined')) STYLE = "btn-normal-primary"
+    if(SIZE.includes('undefined')) SIZE = "btn-md"
 
     return(
         <button 
         type={type} 
         onClick={(onClick)} 
-        className={`btn ${STYLE}`} >{children}{icon}</button>
-
-        // <button
-        // type={type}
-        // onClick={onClick}
-        // variant={variant} 
-        // icon={icon}
-        // color={color}>
-        // {children}
-        // </button>
+        className={`btn ${STYLE} ${SIZE}`} >{children}{icon}</button>
     );
 }
  
