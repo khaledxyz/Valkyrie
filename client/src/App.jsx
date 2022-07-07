53ced257b4355d49e8adba89c1c1ecd94a8d87ab
attano001 <attano001@outlook.com>
2022-07-07 09:54:00 +0100

    Moved React Router from main.jsx to App.jsx.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,7 @@
+// * DEPENDENCIES * //
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AnimatePresence } from 'framer-motion';
+
 // * ROUTES * //
 import Landing from "./routes/Landing/Landing";
 import Login from "./routes/Login/Login";
@@ -7,7 +11,17 @@ import Signup from "./routes/Signup/Signup";
 import './sass/index.scss';
 
 const App = () => {
-  return <Landing />;
+  return (
+    <AnimatePresence>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </AnimatePresence>
+  )
 }
- 
+
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AnimatePresence } from 'framer-motion';
 
 import App from './App'
-import Login from './routes/Login/Login';
-import Signup from './routes/Signup/Signup';
 
 import { Provider } from 'react-redux'
 import { store } from './app/store';
@@ -13,15 +9,7 @@ import { store } from './app/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <AnimatePresence>
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<App />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/signup" element={<Signup />} />
-          </Routes>
-        </BrowserRouter>
-      </AnimatePresence>
+      <App />
     </Provider>
   </React.StrictMode>
 )
