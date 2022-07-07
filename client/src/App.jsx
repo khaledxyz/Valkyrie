4f18439f94e9725380764a3319174a124157efee
attano001 <attano001@outlook.com>
2022-07-07 10:31:09 +0100

    Basic routing.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 // * DEPENDENCIES * //
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AnimatePresence } from 'framer-motion';
 
 // * ROUTES * //
 import Landing from "./routes/Landing/Landing";
+import Dashboard from "./routes/Dashboard/Dashboard";
 import Login from "./routes/Login/Login";
 import Signup from "./routes/Signup/Signup";
 
@@ -11,13 +12,16 @@ import Signup from "./routes/Signup/Signup";
 import './sass/index.scss';
 
 const App = () => {
+  const user = localStorage.getItem("user");
+
   return (
     <AnimatePresence>
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Landing />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/channels" element={user ? <Dashboard /> : <Navigate to="/login"/>}/>
+          <Route exact path="/login" element={!user ? <Login /> : <Navigate to="/channels"/>} />
+          <Route exact path="/signup" element={!user ? <Signup /> : <Navigate to="/channels"/>} />
         </Routes>
       </BrowserRouter>
     </AnimatePresence>
