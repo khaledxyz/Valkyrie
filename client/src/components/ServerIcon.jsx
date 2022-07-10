ee8e0a0a4237bf5a049e49e42d84a75e94b4f1f9
attano001 <attano001@outlook.com>
2022-07-10 19:56:39 +0100

    Create ServerIcon.jsx

---
+++ client/src/components/ServerIcon.jsx
@@ -0,0 +1,33 @@
+
+import styled from 'styled-components';
+
+export const ServerIcon = styled.div`
+    width: 50px;
+    aspect-ratio: 1/1;
+    border-radius: 50%;
+
+    background-color: var(--madison);
+    background-image: url(https://cdn.discordapp.com/avatars/373146874523287552/5586c5af1e2734d5e0077f2b29c6d4b4.webp);
+    background-size: cover;
+    background-position: center;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    // States
+    &:hover{
+        cursor: pointer;
+        border-radius: 30%;
+        background-color: var(--azure-radiance);
+    }
+
+    transition: 0.2s ease;
+`;
+ 
+export const HomeIcon = styled(ServerIcon)`
+        background: var(--madison);
+    &:hover{
+        background: var(--azure-radiance);
+    }
+`
