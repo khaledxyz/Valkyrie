46d685575ca9a8c0d1b901737065d5ef684823d2
attano001 <attano001@outlook.com>
2022-07-10 19:56:57 +0100

    Create ServerList component.

---
+++ client/src/components/ServerList/ServerList.jsx
@@ -0,0 +1,31 @@
+import { ServerIcon, HomeIcon} from '../ServerIcon';
+import './ServerList.scss';
+
+const ServerList = () => {
+    return ( 
+        <aside className='ServerList'>
+            <HomeIcon>V</HomeIcon>
+            <div>--</div>
+            <ServerIcon />
+            <ServerIcon />
+            <ServerIcon />
+            <ServerIcon />
+            <ServerIcon />
+            <ServerIcon />
+            <ServerIcon />
+            <ServerIcon />
+            <ServerIcon />
+            <ServerIcon />
+            <ServerIcon />
+            <ServerIcon />
+            <ServerIcon />
+            <ServerIcon />
+            <ServerIcon />
+            <ServerIcon />
+            <ServerIcon />
+            <ServerIcon />
+        </aside>
+     );
+}
+ 
+export default ServerList;
