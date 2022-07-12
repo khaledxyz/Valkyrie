0c12613cca99b4e755fa3386ac2eddec0475178e
attano001 <attano001@outlook.com>
2022-07-12 23:20:58 +0100

    Update Dashboard.jsx

--- client/src/routes/Dashboard/Dashboard.jsx
+++ client/src/routes/Dashboard/Dashboard.jsx
@@ -2,11 +2,16 @@ import './Dashboard.scss';
 
 import ServerList from "../../components/ServerList/ServerList";
 import Sidebar from '../../components/Sidebar/Sidebar';
+
 const Dashboard = () => {
     return ( 
         <div className='Dashboard'>
             <ServerList />
             <Sidebar />
+
+            <div className="Main-app">
+
+            </div>
         </div>
      );
 }
