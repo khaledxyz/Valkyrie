a3748cfc7f53ea10078224df857020f877d4b75c
attano001 <attano001@outlook.com>
2022-07-13 22:12:40 +0100

    Get single guild info.

--- server/src/routes/guildsRouter.js
+++ server/src/routes/guildsRouter.js
@@ -4,11 +4,12 @@ const router = express.Router();
 const multer = require('multer');
 const upload = multer({ dest: 'valkyrie/uploads' })
 
-const {getGuilds, postGuild, joinGuild} = require('../controllers/guildsController');
+const {getGuild, getGuilds, postGuild, joinGuild} = require('../controllers/guildsController');
 const {authenticate} = require('../middlewares/authMiddleware');
 
 // Fetch guilds
 router.get('/', authenticate, getGuilds);
+router.get('/:id', authenticate, getGuild);
 
 // Create guild
 router.post('/', authenticate, upload.single('serverIcon'), postGuild);
