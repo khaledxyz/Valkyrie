9d7d5f963db34902eb75c7995f93b8192f4438b3
attano001 <attano001@outlook.com>
2022-07-13 22:12:04 +0100

    Added guilds reducer

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from '../features/auth/authSlice';
+import guildsReducer from '../features/guilds/guildsSlice';
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
+        guilds: guildsReducer,
     }
 });
 
