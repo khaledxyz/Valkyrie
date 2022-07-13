bd4a0812cb2286e700e90a7b269168e581335724
attano001 <attano001@outlook.com>
2022-07-13 22:12:57 +0100

    Update Dashboard.jsx

--- client/src/routes/Dashboard/Dashboard.jsx
+++ client/src/routes/Dashboard/Dashboard.jsx
@@ -1,9 +1,27 @@
-import './Dashboard.scss';
+// * DEPENDENCIES * //
+import { useEffect } from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 
+// * REDUX SLICE * //
+import { getAllGuilds } from '../../features/guilds/guildsSlice';
+
+// * COMPONENTS * //
 import ServerList from "../../components/ServerList/ServerList";
 import Sidebar from '../../components/Sidebar/Sidebar';
 
+// * STYLES * //
+import './Dashboard.scss';
+
 const Dashboard = () => {
+    const dispatch = useDispatch();
+
+    const user = useSelector((state) => state.auth.user);
+    const guildsState = useSelector((state) => state.guilds);
+
+    useEffect(() => {
+        dispatch(getAllGuilds());
+    }, []);
+
     return ( 
         <div className='Dashboard'>
             <ServerList />
