aedf286eb0e4361141ac24a44cea77ddefd32cad
attano001 <attano001@outlook.com>
2022-08-05 10:36:16 +0100

    fix: remove unnecessary nested guilds array.

--- server/src/controllers/guildsController.js
+++ server/src/controllers/guildsController.js
@@ -9,47 +9,47 @@ var path = require('path');
 // @desc    get single guild
 // @route   GET /api/guild/:id
 // @access  private
-const getGuild = asyncHandler(async(req, res) => {
+const getGuild = asyncHandler(async (req, res) => {
     const user = await userModel.findById(req.user.id);
 
-    if(!user) {
+    if (!user) {
         res.status(404);
         throw new Error('Not authorized. No Token.');
     };
 
-    if(!user.guilds.includes(req.params.id)) {
+    if (!user.guilds.includes(req.params.id)) {
         res.status(404);
         throw new Error('Server not found.');
     };
 
     const guild = await guildModel.findById(req.params.id);
 
-    res.status(200).json({guild})
+    res.status(200).json({ guild })
 });
 
 // * GET GUILDS * //
 // @desc    get guilds
 // @route   GET /api/guilds
 // @access  private
-const getGuilds = asyncHandler(async(req, res) => {
+const getGuilds = asyncHandler(async (req, res) => {
     const user = await userModel.findById(req.user.id);
 
-    if(!user) {
+    if (!user) {
         res.status(404);
         throw new Error('Not authorized. No Token.');
     }
 
-    res.status(200).json({guilds: user.guilds})
+    res.status(200).json(user.guilds)
 });
 
 // * CREATE GUILD * //
 // @desc    Create guild
 // @route   POST /api/guilds
 // @access  private
-const postGuild = asyncHandler(async(req, res) => {
+const postGuild = asyncHandler(async (req, res) => {
     const user = await userModel.findById(req.user.id);
 
-    if(!user) {
+    if (!user) {
         res.status(404);
         throw new Error('Not authorized. No Token.');
     }
@@ -64,42 +64,42 @@ const postGuild = asyncHandler(async(req, res) => {
         members: [req.user.id]
     });
 
-    await userModel.findOneAndUpdate({_id: req.user.id}, {$push: {guilds: guild._id}});
+    await userModel.findOneAndUpdate({ _id: req.user.id }, { $push: { guilds: guild._id } });
 
-    res.status(201).json({guild});
+    res.status(201).json({ guild });
 });
 
 // * JOIN GUILD * //
 // @desc    Create guild
 // @route   PUT /api/guilds/:id/join
 // @access  private
-const joinGuild = asyncHandler(async(req, res) => {
+const joinGuild = asyncHandler(async (req, res) => {
     const guild = await guildModel.findById(req.params.id)
     const user = await userModel.findById(req.user.id);
 
     // Checks if user exists
-    if(!user) {
+    if (!user) {
         res.status(404);
         throw new Error('Not authorized. No Token.');
     }
 
     // Checks if server exists
-    if(!guild) {
+    if (!guild) {
         res.status(404);
         throw new Error('Could not find server.');
     }
 
     // Checks if user is already in guild
-    if(guild.members.includes(req.user.id)) {
+    if (guild.members.includes(req.user.id)) {
         res.status(400);
         throw new Error('You are already in this server.');
     }
 
     // Adds user to guild
-    await guildModel.findOneAndUpdate({_id: req.params.id}, {$push: {members: req.user.id}});
-    await userModel.findOneAndUpdate({_id: req.user.id}, {$push: {guilds: req.params.id}});
+    await guildModel.findOneAndUpdate({ _id: req.params.id }, { $push: { members: req.user.id } });
+    await userModel.findOneAndUpdate({ _id: req.user.id }, { $push: { guilds: req.params.id } });
 
-    res.status(200).json({message: "Successfully joined server."})
+    res.status(200).json({ message: "Successfully joined server." })
 });
 
 module.exports = { getGuild, getGuilds, postGuild, joinGuild };
