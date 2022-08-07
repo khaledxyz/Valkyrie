0faa664342a286886e6ce7e9c8ca1ee38983cbbe
attano001 <attano001@outlook.com>
2022-08-07 15:49:44 +0100

    fix: undefined token

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -4,27 +4,27 @@ const asyncHandler = require('express-async-handler');
 
 const authenticate = asyncHandler(async (req, res, next) => {
     let token;
-    const bearerToken = req.headers.authorization;
+    const bearerToken = req.headers.authorization || req.body.headers.Authorization;
 
-    if(bearerToken && bearerToken.startsWith('Bearer')) {
+    if (bearerToken && bearerToken.startsWith('Bearer')) {
         try {
             // Gets the token from header
             token = bearerToken.split(' ')[1];
-            
+
             // Verifies the token
             const decodedToken = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
 
             // Gets the user data
             req.user = await userModel.findById(decodedToken.id).select('-password');
             next();
-        } 
+        }
         catch (error) {
             console.log(error);
             res.status(401);
             throw new Error('Not authorized. Invalid token signature.');
         }
     }
-    if(!token) throw new Error('Not authorized. No token.');
+    if (!token) throw new Error('Not authorized. No token.');
 });
 
 module.exports = { authenticate };
