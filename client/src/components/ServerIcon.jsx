1cb8a99ab35127b3a3b0198084eaa3e76b3c0a50
attano001 <attano001@outlook.com>
2022-08-07 21:18:50 +0100

    feat: show guild icons.

--- client/src/components/ServerIcon.jsx
+++ client/src/components/ServerIcon.jsx
@@ -8,7 +8,7 @@ export const ServerIcon = styled.div`
     border-radius: 50%;
 
     background-color: var(--madison);
-    background-image: url(https://cdn.discordapp.com/avatars/373146874523287552/5586c5af1e2734d5e0077f2b29c6d4b4.webp);
+    background-image: ${({ guild }) => `url(${guild})`};
     background-size: cover;
     background-position: center;
 
@@ -25,7 +25,7 @@ export const ServerIcon = styled.div`
 
     transition: 0.2s ease;
 `;
- 
+
 export const HomeIcon = styled(ServerIcon)`
     background: var(--madison);
     font-size: 1.5rem;
