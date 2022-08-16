be1c458e53f00baeff11bfe49ffd8572a0aff0e8
attano001 <attano001@outlook.com>
2022-08-16 17:58:01 +0100

    feat: implement saving messages to MongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -18,16 +18,18 @@ app.use(errorHandler);
 
 // * Connection * //
 app.listen(PORT, () => {
-    console.log('---------------------')
+    console.log('---------------------');
     console.log(
         'Dev server running at  >'.green,
         `https://localhost:${PORT}`.blue
-    )
-    connectDB()
-})
+    );
+    connectDB();
+});
 
 // * Routes * //
 app.use('/api/auth', require('./routes/authRouter'));
 app.use('/api/users', require('./routes/usersRouter'));
 app.use('/api/friend-requests', require('./routes/friendRequestsRouter'));
 app.use('/api/guilds', require('./routes/guildsRouter'));
+app.use('/api/messages/user-messages', require('./routes/userMessagesRouter'));
+// app.use('/api/messages/guild-messages', require('./routes/guildMessagesRouter')); // TODO: Implement guild messages router
