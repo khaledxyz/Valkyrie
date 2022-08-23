7e5a220d7f7e6e9008d9badd1d15daefc076c268
attano001 <attano001@outlook.com>
2022-08-23 17:33:28 +0100

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // * Middleware * //
-app.use(cors({ origin: 'https://valkyrieapp.vercel.app/' }));
+app.use(cors({ origin: 'https://valkyrieapp.vercel.app' }));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(errorHandler);
