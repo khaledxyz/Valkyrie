0298c7ff4d4be35246cc89b6bb4f13206dd73b71
attano001 <attano001@outlook.com>
2022-08-23 17:41:55 +0100

    Update socket.js

--- server/src/socket/socket.js
+++ server/src/socket/socket.js
@@ -1,7 +1,7 @@
 const connectSocket = (server) => {
     const io = require('socket.io')(server, {
         cors: {
-            origin: 'http://localhost:3000'
+            origin: 'https://valkyrieapp.vercel.app/'
         }
     });
 
