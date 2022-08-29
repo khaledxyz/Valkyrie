8202fa58207c147f8f97620fdeaafd1690aea072
attano001 <attano001@outlook.com>
2022-08-29 12:24:37 +0100

    feat: add value to inputs

--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -2,26 +2,27 @@ import { useRef } from 'react';
 
 import './Input.scss';
 
-const Input = ( { type, label, placeholder, required , onChange, error } ) => {
+const Input = ({ type, label, placeholder, required, onChange, error, value }) => {
     const inputRef = useRef();
     let err = null
-    if(error) {
+    if (error) {
         inputRef.current.classList.add('error');
         err = error.response.data.msg
     }
 
-    return ( 
+    return (
         <>
             <h6 className={`label`}>{label}</h6>
-            <input 
-            onChange={onChange} 
-            className={`Input`} 
-            type={type} placeholder={placeholder} 
-            required={required}
-            ref={inputRef}
+            <input
+                onChange={onChange}
+                className={`Input`}
+                type={type} placeholder={placeholder}
+                required={required}
+                ref={inputRef}
+                value={value}
             />
         </>
-     );
+    );
 }
- 
+
 export default Input;
