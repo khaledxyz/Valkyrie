15a962b988d3ce8428a1ecc0fb43f4456b7d3cda
attano001 <attano001@outlook.com>
2022-10-05 17:40:33 +0100

    Update store.js

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,24 +1,15 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from '../features/auth/authSlice';
 import guildsReducer from '../features/guilds/guildsSlice';
 import friendsReducer from '../features/friends/friendsSlice';
 import conversationReducer from '../features/conversation/conversationSlice';
 
-const combinedReducer = combineReducers({
-    auth: authReducer,
-    friends: friendsReducer,
-    conversation: conversationReducer,
-    guilds: guildsReducer,
+export const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        friends: friendsReducer,
+        conversation: conversationReducer,
+        guilds: guildsReducer,
+    }
 });
-
-const rootReducer = (state, action) => {
-    if (action.type === 'auth/logout') { state = undefined; }
-    return combinedReducer(state, action);
-};
-
-const store = configureStore({
-    reducer: rootReducer
-});
-
-export { store };
