82fb91ade56d1ee9c6b62b5ca5c2c53399a24c6f
Khaled <attano001@outlook.com>
2022-10-09 19:23:46 +0100

    redirect to login

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,4 +1,12 @@
+import { useNavigate } from 'react-router-dom';
+
 const Landing = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate('/login');
+    }, []);
+    
     return ( 
         <div className="Valkyrie">
             <h1>Valkyrie</h1>   
