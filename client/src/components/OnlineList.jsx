a2a5da77d7efb0dc89e816da046d30c6255b2f53
attano001 <attano001@outlook.com>
2022-10-10 15:21:40 +0100

    feat: update UI

--- client/src/components/OnlineList.jsx
+++ client/src/components/OnlineList.jsx
@@ -4,6 +4,8 @@ import { Friend } from './FriendsTab/Friend';
 import { ProfileIcon } from './ProfileIcon';
 
 const StyledOnlineList = styled.div`
+    position: relative;
+
     width: 100%;
     height: 100vh;
 
@@ -11,15 +13,34 @@ const StyledOnlineList = styled.div`
 
     text-align: center;
     background-color: var(--maastricht-blue);
+    img{
+        user-select:none;
+        pointer-events:none;
+
+        position: absolute;
+        translate: -50% -50%;
+        top: 50%;
+        left: 50%;
+
+        width: 100px;
+    }
 `;
 
 const OnlineList = ({ online }) => {
     const navigate = useNavigate();
     return (
         <StyledOnlineList>
-            <p>Active Friends</p>
+            <h5>Active Friends</h5>
 
-            {online.map(friend => (
+            {online.length < 1 &&
+                <>
+                    <h6>No one is here ...</h6>
+                    <img src="/issue.svg" />
+                </>
+            }
+
+
+            {online && online.map(friend => (
                 <Friend key={friend._id} onClick={() => navigate(`/channels/@me/${friend._id}`)}>
                     <ProfileIcon avatar={friend.avatar} />
                     <p>{friend.username}</p>
